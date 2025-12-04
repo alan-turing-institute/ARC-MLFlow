5ebdb6d78656502383e543e48d2481ec9d4bb1c5
Jack Roberts <jroberts@turing.ac.uk>
2025-12-04 16:56:13 +0000

    psycopg2

--- container-app/deploy.sh
+++ container-app/deploy.sh
@@ -307,8 +307,8 @@ PGBOUNCER_FQDN=$(az containerapp show \
   --query properties.configuration.ingress.fqdn -o tsv)
 
 # DB connection strings via PgBouncer
-DB_CONNECTION_STRING="postgresql+psycopg://${MLFLOW_DB_USER}:${MLFLOW_DB_USER_PASSWORD}@${PGBOUNCER_FQDN}:5432/${MLFLOW_DB_NAME}"
-AUTH_DB_CONNECTION_STRING="postgresql+psycopg://${AUTH_DB_USER}:${AUTH_DB_USER_PASSWORD}@${PGBOUNCER_FQDN}:5432/${AUTH_DB_NAME}"
+DB_CONNECTION_STRING="postgresql://${MLFLOW_DB_USER}:${MLFLOW_DB_USER_PASSWORD}@${PGBOUNCER_FQDN}:5432/${MLFLOW_DB_NAME}"
+AUTH_DB_CONNECTION_STRING="postgresql://${AUTH_DB_USER}:${AUTH_DB_USER_PASSWORD}@${PGBOUNCER_FQDN}:5432/${AUTH_DB_NAME}"
 
 echo "📡 PgBouncer deployed at: ${PGBOUNCER_FQDN}"
 
